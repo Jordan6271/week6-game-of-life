333550e953d6b07233aac799f81a6a22baf1b109
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-03-07 18:08:00 +0000

    Add tests for second and fourth rules and made second rule pass with function

--- verifier.spec.js
+++ verifier.spec.js
@@ -1,17 +1,49 @@
 import {golTest} from './verifier';
 
 //Any live cell with fewer than two live neighbours dies as if by under population.
-describe("queryOne", () => {
+describe("game of life rules", () => {
     it("should die with fewer than 2 live neighbours", () => {
         let before = [
             [0, 1, 0],
             [0, 1, 1],
-            [1, 0, 0],
+            [1, 0, 1],
         ];
         let after =  [
-            [0, 1, 0],
             [0, 1, 1],
-            [0, 0, 0],
+            [1, 0, 1],
+            [0, 0, 1],
+        ];
+        const result = golTest(before);
+        expect(result).toStrictEqual(after);
+    })
+
+    it("should die with more than 3 live neighbours", () => {
+        let before = [
+            [0, 1, 1],
+            [1, 1, 1],
+            [1, 0, 0],
+        ];
+        let after = [
+            [1, 0, 1],
+            [1, 0, 1],
+            [1, 0, 0],
+        ];
+        const result = golTest(before);
+        expect(result).toStrictEqual(after);
+    })
+
+    // Isn't point 3 already tested by the prior 2 tests due to the fact it outputs an array?
+
+    it("should become a live cell if dead with exactly 3 neighbours", () => {
+        let before = [
+            [0, 1, 1],
+            [1, 1, 1],
+            [1, 0, 1],
+        ];
+        let after = [
+            [1, 0, 1],
+            [1, 0, 0],
+            [1, 0, 1],
         ];
         const result = golTest(before);
         expect(result).toStrictEqual(after);
