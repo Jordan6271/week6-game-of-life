c14389d8e7ca201f2b7c7593a28149765d5bc431
markh1089 <75758620+markh1089@users.noreply.github.com>
2021-03-16 16:23:42 +0000

    marks variation

--- verifier.js
+++ verifier.js
@@ -1,32 +1,98 @@
-function golTest(input) {
-    const resolution = 3;
-    function getNeighbourCount (x, y) {
-        let count = 0;
-        for (let j = -1; j < 2; j++) {
-            for (let i = -1; i < 2; i++) {
-                if (i === 0 && j === 0 || x + i < 0 || x + i > resolution - 1 || y + j < 0 || y + j > resolution - 1) continue;
-                if (input[x + i][y + j] === 1) count ++;
-            }
-        }
-        return count;
+function isAlive(cell, neighbours) {
+    if (neighbours === 3 || cell === 1 && neighbours === 2) {
+        return 1;
+    } else {
+        return 0;
     }
-    
-    function step() {
-        let newArr = [
-            [1, 1, 1],
-            [1, 1, 1],
-            [1, 1, 1],
-        ];
-        for (let y = 0; y < resolution; y++) {
-            for (let x = 0; x < resolution; x++) {
-                const neighbours = getNeighbourCount(x, y);
-                if (input[x][y] == 1 && neighbours < 2 || input[x][y] == 1 && neighbours > 3) newArr[x][y] = 0;
-                if (input[x][y] == 0) newArr[x][y] = 0;
-            }
-        }
-        return newArr
-    }
-        return step();  
 }
 
-export {golTest};
+function generateGrid(root) {
+    return new Array(root * root).fill(0)
+}
+//console.log(generateGrid(5))
+
+function neighbourCount(cells, index) {
+    const width = Math.sqrt(cells.length);
+    const result = 
+    (cells[index - 1] || 0) +
+    (cells[index + 1] || 0) + 
+    (cells[index - width] || 0) +
+    (cells[index - width -1] || 0) + 
+    (cells[index + width] || 0) +
+    (cells[index + width +1] || 0);
+    return result;
+    
+}
+console.log(neighbourCount([1,1,1,0,0,0,0,0,0],4))
+
+function liveDie(cellState) {
+    return cellState.map(cell => isAlive(cell, 0));
+
+}
+//console.log(liveDie([1,1,1]))
+
+// export { isAlive }
+// export { generateGrid }
+// export { liveDie }
+// export {neighbourCount}
+
+// function neighbourCount(neighbour) {
+//     if (neighbour < 2) {
+//         return "dead";
+//     } else {
+//         return "alive";
+//     }
+// }
+// function neighbourCount(num) {
+//     let count = 0;
+//     for (let i = 0; i < num.length; i++) {
+//         if (num[i] === 1) {
+//             count++
+//         }
+//     } if (count < 2) {
+//         return 'dead';
+//     } else {
+//         return 'alive';
+//     }
+// }
+
+
+
+
+//export { neighbourCount }
+
+
+
+
+// function golTest(input) {
+//     const resolution = 3;
+//     function getNeighbourCount (x, y) {
+//         let count = 0;
+//         for (let j = -1; j < 2; j++) {
+//             for (let i = -1; i < 2; i++) {
+//                 if (i === 0 && j === 0 || x + i < 0 || x + i > resolution - 1 || y + j < 0 || y + j > resolution - 1) continue;
+//                 if (input[x + i][y + j] === 1) count ++;
+//             }
+//         }
+//         return count;
+//     }
+
+//     function step() {
+//         let newArr = [
+//             [1, 1, 1],
+//             [1, 1, 1],
+//             [1, 1, 1],
+//         ];
+//         for (let y = 0; y < resolution; y++) {
+//             for (let x = 0; x < resolution; x++) {
+//                 const neighbours = getNeighbourCount(x, y);
+//                 if (input[x][y] == 1 && neighbours < 2 || input[x][y] == 1 && neighbours > 3) newArr[x][y] = 0;
+//                 if (input[x][y] == 0) newArr[x][y] = 0;
+//             }
+//         }
+//         return newArr
+//     }
+//         return step();
+// }
+
+// export {golTest};
--- verifier.spec.js
+++ verifier.spec.js
@@ -1,4 +1,5 @@
-import {golTest} from './verifier';
+
+import { isAlive, generateGrid, liveDie, neighbourCount} from './verifier';
 
 // Rules of the Game of Life
 // 1. Any live cell with fewer than two live neighbours dies as if by under population.
@@ -6,54 +7,131 @@ import {golTest} from './verifier';
 // 3. Any live cell with two or three live neighbours lives on to the next generation.
 // 4. Any dead cell with exactly three live neighbours becomes a live cell.
 
-describe("game of life rules", () => {
-    // Any live cell with fewer than two live neighbours dies as if by under population.
-    it("should die with fewer than 2 live neighbours", () => {
-        let before = [
-            [0, 1, 0],
-            [0, 1, 1],
-            [1, 0, 1],
-        ];
-        let after =  [
-            [0, 1, 1],
-            [1, 0, 1],
-            [0, 0, 1],
-        ];
-        const result = golTest(before);
-        expect(result).toStrictEqual(after);
+describe('Game of life rules', () => {
+    describe('isAlive', () => {
+        it('is dead without neighbours', () => {
+            expect(isAlive(0, 0)).toEqual(0);
+        })
+        it('is dead and has 3 live neighbours should live(1)', () => {
+            expect(isAlive(0, 3)).toEqual(1)
+        })
+        it('live cell with 0 neighbours dies(0)', () => {
+            expect(isAlive(1, 0)).toEqual(0)
+
+        })
+        it('live cell with 2 neighbours lives (1)', () => {
+            expect(isAlive(1, 2)).toEqual(1)
+        })
+    })
+    describe('grid', () => {
+        it('Should generate an array of input * input', () => {
+            expect(generateGrid(1)).toEqual([0]);
+            expect(generateGrid(2)).toEqual([0, 0, 0, 0])
+        })
+    })
+    ///for below width is determined by square root of length of array - i.e. an array of 4 numbers sqrt = 2 (2x2 grid) :
+    //[1,1]
+    //[1,0]
+    describe('neighbour count', () => {
+        it('should count 1 for an array of one', () => {
+            expect(neighbourCount([1], 0)).toEqual(0);
+        })
+        it('should count 2 neighbours', () => {
+            expect(neighbourCount([1,1,1,0],0)).toEqual(2)
+           
+        })
+        it('should count 2 neighbours', () => {
+            expect(neighbourCount([1,1,1,0],1)).toEqual(2)
+        })
+        it('should count 2 neighbours', () => {
+            expect(neighbourCount([1,1,1,0],2)).toEqual(2)
+        })
+        it('should count 3 neighbours', () => {
+            expect(neighbourCount([1,1,1,0],3)).toEqual(3)
+        })
+        it('should count 3 neighbours', () => {
+            expect(neighbourCount([1,1,1,0,0,0,0,0,0],4)).toEqual(3)
+            //[1,1,1]
+            //[0,0,0]
+            //[0,0,0]
+        })
     })
 
-    // Any live cell with more than three live neighbours dies, as if by overcrowding.
-    it("should die with more than 3 live neighbours", () => {
-        let before = [
-            [0, 1, 1],
-            [1, 1, 1],
-            [1, 0, 0],
-        ];
-        let after = [
-            [1, 0, 1],
-            [1, 0, 1],
-            [1, 0, 0],
-        ];
-        const result = golTest(before);
-        expect(result).toStrictEqual(after);
+    describe('live/die', () => {
+        it('does not update dead (0) cells', () => {
+            const cellState = generateGrid(1);
+            expect(liveDie(cellState)).toEqual(cellState)
+        })
+        it('returns all dead cells', () => {
+            const initialCellState = generateGrid(2);
+            const cellState = generateGrid(2);
+            cellState[0] = 1;
+            expect(liveDie(cellState)).toEqual(initialCellState)
+        })
+        xit('should return all live cells(1)', () => {
+            const cellState = [1,1,1,0];
+            expect(liveDie(cellState)).toEqual([1,1,1,1])
+        })
     })
 
-    // Isn't point 3 already tested by the prior 2 tests due to the fact it outputs an array?
-
-    // Any dead cell with exactly three live neighbours becomes a live cell.
-    it("should become a live cell if dead with exactly 3 neighbours", () => {
-        let before = [
-            [0, 1, 1],
-            [1, 1, 1],
-            [1, 0, 1],
-        ];
-        let after = [
-            [1, 0, 1],
-            [1, 0, 0],
-            [1, 0, 1],
-        ];
-        const result = golTest(before);
-        expect(result).toStrictEqual(after);
-    })
 })
+//     it('any cell with less than two live neighbours dies', () => {
+//         let cellState = [[0,1,1,1]]
+//         const cellArray = neighbourCount(cellState);
+//         expect(cellArray).toBe('alive');
+
+//     })
+// })
+
+
+// describe("game of life rules", () => {
+//     // Any live cell with fewer than two live neighbours dies as if by under population.
+//     it("should die with fewer than 2 live neighbours", () => {
+//         let before = [
+//             [0, 1, 0],
+//             [0, 1, 1],
+//             [1, 0, 1],
+//         ];
+//         let after =  [
+//             [0, 1, 0],
+//             [0, 1, 1],
+//             [0, 0, 1],
+//         ];
+//         const result = golTest(before);
+//         expect(result).toStrictEqual(after);
+//     })
+
+//     // Any live cell with more than three live neighbours dies, as if by overcrowding.
+//     it("should die with more than 3 live neighbours", () => {
+//         let before = [
+//             [0, 1, 1],
+//             [1, 1, 1],
+//             [1, 0, 0],
+//         ];
+//         let after = [
+//             [1, 0, 1],
+//             [1, 0, 1],
+//             [1, 0, 0],
+//         ];
+//         const result = golTest(before);
+//         expect(result).toStrictEqual(after);
+//     })
+
+//     // Isn't point 3 already tested by the prior 2 tests due to the fact it outputs an array?
+
+//     // Any dead cell with exactly three live neighbours becomes a live cell.
+//     it("should become a live cell if dead with exactly 3 neighbours", () => {
+//         let before = [
+//             [0, 1, 1],
+//             [1, 1, 1],
+//             [1, 0, 1],
+//         ];
+//         let after = [
+//             [1, 0, 1],
+//             [1, 0, 0],
+//             [1, 0, 1],
+//         ];
+//         const result = golTest(before);
+//         expect(result).toStrictEqual(after);
+//     })
+// })
