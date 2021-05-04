facb0c7c87371d2de344ea6ae6c27004e9f75550
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-05-04 20:44:33 +0100

    Fix indentation

--- verifier.js
+++ verifier.js
@@ -1,107 +1,107 @@
 const resolution = 3;
 function getNeighbourCount(x, y, input) {
-	let count = 0;
-	for (let j = -1; j < 2; j++) {
-		for (let i = -1; i < 2; i++) {
-			if (
-				(i === 0 && j === 0) ||
-				x + i < 0 ||
-				x + i > resolution - 1 ||
-				y + j < 0 ||
-				y + j > resolution - 1
-			)
-				continue;
-			if (input[x + i][y + j] === 1) count++;
-		}
-	}
-	return count;
+    let count = 0;
+    for (let j = -1; j < 2; j++) {
+        for (let i = -1; i < 2; i++) {
+            if (
+                (i === 0 && j === 0) ||
+                x + i < 0 ||
+                x + i > resolution - 1 ||
+                y + j < 0 ||
+                y + j > resolution - 1
+            )
+                continue;
+            if (input[x + i][y + j] === 1) count++;
+        }
+    }
+    return count;
 }
 
 function firstRule(input) {
-	let newArr = [
-		[1, 1, 1],
-		[1, 1, 1],
-		[1, 1, 1],
-	];
-	for (let y = 0; y < resolution; y++) {
-		for (let x = 0; x < resolution; x++) {
-			const neighbours = getNeighbourCount(x, y, input);
-			if (input[x][y] == 1 && neighbours < 2) newArr[x][y] = 0;
-			if (input[x][y] == 0) newArr[x][y] = 0;
-		}
-	}
-	return newArr;
+    let newArr = [
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1],
+    ];
+    for (let y = 0; y < resolution; y++) {
+        for (let x = 0; x < resolution; x++) {
+            const neighbours = getNeighbourCount(x, y, input);
+            if (input[x][y] == 1 && neighbours < 2) newArr[x][y] = 0;
+            if (input[x][y] == 0) newArr[x][y] = 0;
+        }
+    }
+    return newArr;
 }
 
 function secondRule(input) {
-	let newArr = [
-		[1, 1, 1],
-		[1, 1, 1],
-		[1, 1, 1],
-	];
-	for (let y = 0; y < resolution; y++) {
-		for (let x = 0; x < resolution; x++) {
-			const neighbours = getNeighbourCount(x, y, input);
-			if (input[x][y] == 1 && neighbours > 3) newArr[x][y] = 0;
-			if (input[x][y] == 0) newArr[x][y] = 0;
-		}
-	}
-	return newArr;
+    let newArr = [
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1],
+    ];
+    for (let y = 0; y < resolution; y++) {
+        for (let x = 0; x < resolution; x++) {
+            const neighbours = getNeighbourCount(x, y, input);
+            if (input[x][y] == 1 && neighbours > 3) newArr[x][y] = 0;
+            if (input[x][y] == 0) newArr[x][y] = 0;
+        }
+    }
+    return newArr;
 }
 
 function thirdRule(input) {
-	let newArr = [
-		[1, 1, 1],
-		[1, 1, 1],
-		[1, 1, 1],
-	];
-	for (let y = 0; y < resolution; y++) {
-		for (let x = 0; x < resolution; x++) {
-			const neighbours = getNeighbourCount(x, y, input);
-			if (
-				(input[x][y] == 1 && neighbours < 2) ||
-				(input[x][y] == 1 && neighbours > 3)
-			)
-				newArr[x][y] = 0;
-			if (input[x][y] == 0) newArr[x][y] = 0;
-		}
-	}
-	return newArr;
+    let newArr = [
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1],
+    ];
+    for (let y = 0; y < resolution; y++) {
+        for (let x = 0; x < resolution; x++) {
+            const neighbours = getNeighbourCount(x, y, input);
+            if (
+                (input[x][y] == 1 && neighbours < 2) ||
+                (input[x][y] == 1 && neighbours > 3)
+            )
+                newArr[x][y] = 0;
+            if (input[x][y] == 0) newArr[x][y] = 0;
+        }
+    }
+    return newArr;
 }
 
 function fourthRule(input) {
-	let newArr = [
-		[1, 1, 1],
-		[1, 1, 1],
-		[1, 1, 1],
-	];
-	for (let y = 0; y < resolution; y++) {
-		for (let x = 0; x < resolution; x++) {
-			const neighbours = getNeighbourCount(x, y, input);
-			if (input[x][y] == 0 && neighbours !== 3) newArr[x][y] = 0;
-		}
-	}
-	return newArr;
+    let newArr = [
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1],
+    ];
+    for (let y = 0; y < resolution; y++) {
+        for (let x = 0; x < resolution; x++) {
+            const neighbours = getNeighbourCount(x, y, input);
+            if (input[x][y] == 0 && neighbours !== 3) newArr[x][y] = 0;
+        }
+    }
+    return newArr;
 }
 
 function fullRules(input) {
-	let newArr = [
-		[1, 1, 1],
-		[1, 1, 1],
-		[1, 1, 1],
-	];
-	for (let y = 0; y < resolution; y++) {
-		for (let x = 0; x < resolution; x++) {
-			const neighbours = getNeighbourCount(x, y, input);
-			if (
-				(input[x][y] == 1 && neighbours < 2) ||
-				(input[x][y] == 1 && neighbours > 3)
-			)
-				newArr[x][y] = 0;
-			if (input[x][y] == 0 && neighbours !== 3) newArr[x][y] = 0;
-		}
-	}
-	return newArr;
+    let newArr = [
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1],
+    ];
+    for (let y = 0; y < resolution; y++) {
+        for (let x = 0; x < resolution; x++) {
+            const neighbours = getNeighbourCount(x, y, input);
+            if (
+                (input[x][y] == 1 && neighbours < 2) ||
+                (input[x][y] == 1 && neighbours > 3)
+            )
+                newArr[x][y] = 0;
+            if (input[x][y] == 0 && neighbours !== 3) newArr[x][y] = 0;
+        }
+    }
+    return newArr;
 }
 
 export { firstRule, secondRule, thirdRule, fourthRule, fullRules };
--- verifier.spec.js
+++ verifier.spec.js
@@ -1,9 +1,9 @@
 import {
-	firstRule,
-	secondRule,
-	thirdRule,
-	fourthRule,
-	fullRules,
+    firstRule,
+    secondRule,
+    thirdRule,
+    fourthRule,
+    fullRules,
 } from "./verifier";
 
 // Rules of the Game of Life
@@ -13,83 +13,83 @@ import {
 // 4. Any dead cell with exactly three live neighbours becomes a live cell.
 
 describe("game of life rules", () => {
-	// Any live cell with fewer than two live neighbours dies as if by under population.
-	it("should die with fewer than 2 live neighbours", () => {
-		let before = [
-			[0, 1, 0],
-			[0, 1, 1],
-			[1, 0, 1],
-		];
-		let after = [
-			[0, 1, 0],
-			[0, 1, 1],
-			[0, 0, 1],
-		];
-		const result = firstRule(before);
-		expect(result).toStrictEqual(after);
-	});
+    // Any live cell with fewer than two live neighbours dies as if by under population.
+    it("should die with fewer than 2 live neighbours", () => {
+        let before = [
+            [0, 1, 0],
+            [0, 1, 1],
+            [1, 0, 1],
+        ];
+        let after = [
+            [0, 1, 0],
+            [0, 1, 1],
+            [0, 0, 1],
+        ];
+        const result = firstRule(before);
+        expect(result).toStrictEqual(after);
+    });
 
-	// Any live cell with more than three live neighbours dies, as if by overcrowding.
-	it("should die with more than 3 live neighbours", () => {
-		let before = [
-			[0, 1, 1],
-			[1, 1, 1],
-			[1, 0, 0],
-		];
-		let after = [
-			[0, 0, 1],
-			[1, 0, 1],
-			[1, 0, 0],
-		];
-		const result = secondRule(before);
-		expect(result).toStrictEqual(after);
-	});
+    // Any live cell with more than three live neighbours dies, as if by overcrowding.
+    it("should die with more than 3 live neighbours", () => {
+        let before = [
+            [0, 1, 1],
+            [1, 1, 1],
+            [1, 0, 0],
+        ];
+        let after = [
+            [0, 0, 1],
+            [1, 0, 1],
+            [1, 0, 0],
+        ];
+        const result = secondRule(before);
+        expect(result).toStrictEqual(after);
+    });
 
-	// Any live cell with two or three live neighbours lives on to the next generation.
-	it("should live if number of neigbours is 2 or 3, otherwise die", () => {
-		let before = [
-			[0, 1, 1],
-			[1, 1, 1],
-			[1, 0, 0],
-		];
-		let after = [
-			[0, 0, 1],
-			[1, 0, 1],
-			[1, 0, 0],
-		];
-		const result = thirdRule(before);
-		expect(result).toStrictEqual(after);
-	});
+    // Any live cell with two or three live neighbours lives on to the next generation.
+    it("should live if number of neigbours is 2 or 3, otherwise die", () => {
+        let before = [
+            [0, 1, 1],
+            [1, 1, 1],
+            [1, 0, 0],
+        ];
+        let after = [
+            [0, 0, 1],
+            [1, 0, 1],
+            [1, 0, 0],
+        ];
+        const result = thirdRule(before);
+        expect(result).toStrictEqual(after);
+    });
 
-	// Any dead cell with exactly three live neighbours becomes a live cell.
-	it("should become a live cell if dead with exactly 3 neighbours", () => {
-		let before = [
-			[0, 1, 1],
-			[1, 1, 1],
-			[1, 0, 1],
-		];
-		let after = [
-			[1, 1, 1],
-			[1, 1, 1],
-			[1, 0, 1],
-		];
-		const result = fourthRule(before);
-		expect(result).toStrictEqual(after);
-	});
+    // Any dead cell with exactly three live neighbours becomes a live cell.
+    it("should become a live cell if dead with exactly 3 neighbours", () => {
+        let before = [
+            [0, 1, 1],
+            [1, 1, 1],
+            [1, 0, 1],
+        ];
+        let after = [
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 0, 1],
+        ];
+        const result = fourthRule(before);
+        expect(result).toStrictEqual(after);
+    });
 
-	// Putting all the rules together.
-	it("should die if fewer than 2 neighbours or more than 3 neighbours, and become alive if dead with exactly 3 neighbours", () => {
-		let before = [
-			[0, 1, 1],
-			[1, 1, 1],
-			[1, 0, 1],
-		];
-		let after = [
-			[1, 0, 1],
-			[1, 0, 0],
-			[1, 0, 1],
-		];
-		const result = fullRules(before);
-		expect(result).toStrictEqual(after);
-	});
+    // Putting all the rules together.
+    it("should die if fewer than 2 neighbours or more than 3 neighbours, and become alive if dead with exactly 3 neighbours", () => {
+        let before = [
+            [0, 1, 1],
+            [1, 1, 1],
+            [1, 0, 1],
+        ];
+        let after = [
+            [1, 0, 1],
+            [1, 0, 0],
+            [1, 0, 1],
+        ];
+        const result = fullRules(before);
+        expect(result).toStrictEqual(after);
+    });
 });
